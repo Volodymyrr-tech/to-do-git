4495b5dde80d363ba868d387feede8eda8f3254d
Volodymyrr-tech <volodymyrr.tch@gmail.com>
2023-06-07 16:05:08 +0300

    Updated configuration files

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,29 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react-swc'
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   build: {
+//     outDir: 'docs',
+//     base: '/to-do-git/',
+//   },
+// })
+
+// import { defineConfig } from 'vite'
+// import reactRefresh from '@vitejs/plugin-react-refresh'
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [reactRefresh()],
+//   base: '/to-do-git/',
+// })
+
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/to-do-git/',
   plugins: [react()],
-  build: {
-    outDir: 'docs',
-    base: '/to-do-git/',
-  },
 })
