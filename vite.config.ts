d2b872e9f97eaef1ed9795fa7ce7a480ecd9e013
Volodymyrr-tech <volodymyrr.tch@gmail.com>
2023-06-08 09:22:47 +0300

    Build for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/to-do-git/' : '/',
+  base: '/to-do-git/',
   plugins: [react()],
 })
